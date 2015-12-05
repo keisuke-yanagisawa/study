34495d8bae5450c006661c802e5c0675c2d5cdda
Keisuke Yanagisawa <yanagisawa@bi.cs.titech.ac.jp>
2015-12-05 19:26:42 +0900

    param tuningを任意の環境で行えるようにし、それにしたがってtaskを作成した。

--- luigi/param_tuning.py
+++ luigi/param_tuning.py
@@ -1,5 +1,8 @@
+import re
+
 import luigi
 import numpy as np
+import pandas as pd
 
 from sklearn import datasets, svm, cross_validation
 
@@ -17,6 +20,53 @@ def flatten_array(array):
             ret.append(a)
     return ret;
 
+def convert2num(c):
+    # https://github.com/pydata/pandas/issues/9589                                                                     
+    try:
+        return c.astype(np.number)
+    except:
+        return c
+
+# common task class for parameter tuning.
+class param_tuning(luigi.Task):
+    tasks        = luigi.Parameter()
+    text_format  = luigi.Parameter()
+    reduce_pivot = luigi.Parameter()
+    reduce_rule  = luigi.Parameter(default="min")
+    out_file     = luigi.Parameter()
+
+    def requires(self):
+        return self.tasks;
+    def output(self):
+        return luigi.LocalTarget(self.out_file)
+    def run(self):
+        results = []
+        for task in self.requires():
+            with task.output().open() as taskfile:
+                string = taskfile.read()
+                groupdict = re.search(self.text_format, string).groupdict()
+                results.append(groupdict)
+
+        # making pandas dataframe
+        df = pd.DataFrame.from_dict(results);
+        df[self.reduce_pivot] = convert2num(df[self.reduce_pivot])
+        values = df[self.reduce_pivot]
+
+        # Arrangement of column order
+        column_order = filter(lambda key: key != self.reduce_pivot, df.columns) + [self.reduce_pivot]
+        df = df[column_order]
+
+        if self.reduce_rule == "min":
+            best_val = min(values)
+        elif self.reduce_rule == "max":
+            best_val = max(values)
+        else:
+            print("reduce_rule must be min or max. your input is %s" % self.reduce_rule)
+            exit(1);
+
+        df[df[self.reduce_pivot] == best_val].to_csv(self.output().fn, index=False);
+
+
 class task_param_eval(luigi.Task):
     data = luigi.Parameter()
     C = luigi.FloatParameter()
@@ -33,37 +83,31 @@ class task_param_eval(luigi.Task):
         # So, the result is inverse of error value.
         results = -cross_validation.cross_val_score(model, data.data, data.target, scoring="mean_absolute_error")
         with self.output().open("w") as out_file:
-            out_file.write( str(np.mean(results)) ); 
-        
+            out_string = "%f,%f,%f" % (self.C, self.gamma, np.mean(results))
+            out_file.write(out_string); 
 
-class task_param_tuning(luigi.Task):
-
-    cost_list = luigi.Parameter(default="1,2,5,10")
-    gamma_list = luigi.Parameter(default="1,2,5,10")
+class main_task(luigi.Task):
+    cost_list = luigi.Parameter(default="1,2,5,10,20,50,100,200,500,1000,2000,5000,10000")
+    gamma_list = luigi.Parameter(default="1,2,5,10,20,50,100,200,500,1000,2000,5000,10000")
     
     data = datasets.load_diabetes()
 
     def requires(self):
-        return flatten_array(
-            map(lambda C:
-                    map(lambda gamma:
-                            task_param_eval(data=frozenset(self.data), # values should be hashable 
-                                       C=float(C), gamma=float(gamma)),
-                        self.cost_list.split(",")),
-                self.gamma_list.split(",")))
-    def output(self):
-        return luigi.LocalTarget("results.csv")
-    def run(self):
+        costs  = self.cost_list.split(",")
+        gammas = self.gamma_list.split(",")
 
-        results = {}
+        tasks = flatten_array(
+            map(lambda C: map(lambda gamma:
+                    task_param_eval(data=frozenset(self.data), # values should be hashable 
+                                    C=float(C), gamma=float(gamma)),
+                costs), gammas))
 
-        for task in self.requires():
-            with task.output().open() as taskfile:
-                results[(task.C, task.gamma)] = float(taskfile.read())
-        
-        best_key = min(results,  key=results.get)
-        with self.output().open("w") as out_file:
-            out_file.write("%s,%s,%.4f\n" %(best_key[0], best_key[1], results[best_key]))
+        s = "[-+]?\d*\.\d+|\d+" ## float or int expression
+        return param_tuning(tasks        = tasks,
+                            text_format  = "(?P<cost>"+s+"),(?P<gamma>"+s+"),(?P<error>"+s+")",
+                            reduce_pivot = "error",
+                            reduce_rule  = "min",
+                            out_file     = "result.csv")
 
 if __name__ == "__main__":
     luigi.run()
